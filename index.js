4e789aee61a88db922c39aceb82ae07a2cd13d1b
paula <paulalaorga@gmail.com>
2023-09-11 18:59:57 +1000

    index

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function(textChange) {
+    const element = document.getElementById("text");
+    element.textContent = 'This is really cool!';
+    console.log(textChange());
+});// Your code goes here
